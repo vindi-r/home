9a22696623d86c4dc4ada1792df2d952de6665d6
Grigory Petrov <grigory.v.p@gmail.com>
2015-08-30 09:45:59 +0300

    bugfix

--- .install_linux.sh
+++ .install_linux.sh
@@ -3,11 +3,8 @@
 cp -r . ~
 cd ~
 . ~/.bashrc
-<<<<<<< HEAD
 ##! Required by easy_install
 mkdir -p ~/.local/python-site-packages/
-##  Works without dudo due to ~/.pydistutils.cfg and PYTHONPATH
+##  Works without sudo due to ~/.pydistutils.cfg and PYTHONPATH
 easy_install pip
-=======
->>>>>>> 071676d79dea9c9941455bdc669e81b317427a7a
 
